1d6d9710507993303c335f9c17ca0093dbeda5d6
giuvallente <giuliagv@al.insper.edu.br>
2024-02-20 09:56:42 -0300

    nivel 1 - versão primitiva

--- nivel1.py
+++ nivel1.py
@@ -10,11 +10,12 @@ FPS = 60  # Frames per Second
 
 BLACK = (0, 0, 0)
 COR_PERSONAGEM = (30, 200, 20)
+COR_PREMIO = (255, 255, 0)
 COR_PLANETA = (255, 0, 0)
 
 # Inicializar posicoes
-s0 = np.array([200,200])
-v0 = np.array([10, -10])
+s0 = np.array([50,200])
+v0 = np.array([0, 0])
 a = np.array([0, 0.2])
 v = v0
 s = s0
@@ -23,38 +24,51 @@ s = s0
 personagem = pygame.Surface((5, 5))  # Tamanho do personagem
 personagem.fill(COR_PERSONAGEM)  # Cor do personagem
 
+clicou = False
 
 rodando = True
 while rodando:
     # Capturar eventos
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pos_mouse = event.pos
+            v = (pos_mouse - s0) * 1/np.linalg.norm(pos_mouse - s0)
+            v = v * 10 
+            clicou = True
+            
 
     if s[0]<10 or s[0]>390 or s[1]<10 or s[1]>390: # Se eu chegar ao limite da tela, reinicio a posição do personagem
-        pos_mouse = pygame.mouse.get_pos()
         v = (pos_mouse - s0) * 1/np.linalg.norm(pos_mouse - s0)
-        rnd = np.random.randn(2)
-        v = v * 10 + rnd
+        v = v * 10
         s, v = s0, v
+        clicou = False
 
     # Controlar frame rate
     clock.tick(FPS)
 
     # Processar posicoes
     pos_planeta = np.array([300, 100])
-    vetor = (pos_planeta - s) * 1/np.linalg.norm(pos_planeta - s) 
 
-    a = (5000 / (np.linalg.norm(pos_planeta - s)) ** 2) * vetor
-    v = v + a 
-    s = s + 0.1 * v 
-    
+    if clicou:
 
-    # Desenhar fundo
+        vetor = (pos_planeta - s) * 1/np.linalg.norm(pos_planeta - s) 
+        a = (1000 / (np.linalg.norm(pos_planeta - s)) ** 2) * vetor
+        v = v + a 
+        s = s + 0.1 * v 
+
+    # Desenha fundo
     screen.fill(BLACK)
+
+    # Desenha planeta
     pygame.draw.circle(screen, COR_PLANETA, (300, 100), 20, 1)
 
-    # Desenhar personagem
+    # Desenha prêmio
+    pygame.draw.circle(screen, COR_PREMIO, (350, 200), 10, 1)
+
+    # Desenha personagem
     rect = pygame.Rect(s, (10, 10))  # First tuple is position, second is size.
     screen.blit(personagem, rect)
 
